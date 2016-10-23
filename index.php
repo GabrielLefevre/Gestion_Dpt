ee65a736535d8fd1fd21610780df912c4b284ec2
GabrielLefevre <lfv.gabriel@gmail.com>
2016-10-23 17:20:26 +0200

    WIP disponibilites

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+</head>
+<body>
+
 <?php
 try
 {
@@ -8,10 +16,122 @@ catch (Exception $e)
     die('Erreur : ' . $e->getMessage());
 }
 
-$reponse = $bdd->query('SELECT * FROM subject LIMIT 0,5');
+
+function listeProfs() {
+    global $bdd;
+    $filename = 'professeurs.xml';
+    if(!file_exists($filename)) {
+        fopen("professeurs.xml","w+");
+        echo "création fichier professeurs.xml".PHP_EOL;
+    }
+    file_put_contents('professeurs.xml', '');
+
+    $req = $bdd->query('SELECT name FROM teacher WHERE dept ="INFO" ');
+    $xml = '<Teachers_List>'.PHP_EOL;
+    while($ligne = $req->fetch()){
+        $xml.="<Teacher>".PHP_EOL."    <Name>".$ligne['name']."</Name>".PHP_EOL."</Teacher>".PHP_EOL;
+    }
+    $xml.="</Teachers_List>".PHP_EOL;
+    file_put_contents("professeurs.xml",$xml);
+
+}// listeProfs
+
+function listeMatieres() {
+    global $bdd;
+    $filename = 'matieres.xml';
+    if(!file_exists($filename)) {
+        fopen("matieres.xml","w+");
+        echo "création fichier matieres.xml".PHP_EOL;
+    }
+    file_put_contents('matieres.xml', '');
+
+    $req = $bdd->query('SELECT name FROM subject JOIN course ON subject.id=course.subject_id WHERE course.dept="INFO" AND course.semester="S5" ');
+    $xml = '<Subjects_List>'.PHP_EOL;
+    while($ligne = $req->fetch()){
+        $xml.="<Subject>".PHP_EOL."    <Name>".$ligne['name']."</Name>".PHP_EOL."</Subject>".PHP_EOL;
+    }
+    $xml.="</Subjects_List>".PHP_EOL;
+    file_put_contents("matieres.xml",$xml);
+
+}// listeMatieres
+
+function listeDispoProfs() {
+    global $bdd;
+    $filename = 'dispo.xml';
+    if(!file_exists($filename)) {
+        fopen("dispo.xml","w+");
+        echo "création fichier dispo.xml".PHP_EOL;
+    }
+    file_put_contents('dispo.xml', '');
+
+    $req = $bdd->query('SELECT t.name, d.* FROM teacher t JOIN disponibilite d ON t.id=d.teacher_id WHERE t.dept="INFO" ');
+    $xml='<ConstraintTeacherNotAvailableTimes>'.PHP_EOL.'    <Weight_Percentage>100</Weight_Percentage>'.PHP_EOL;
+
+    $tmp ="";
+    $nbIndispo=0;
+    $tabDispo = array();
+    $tabJour=array('Lundi','Mardi','Mercredi','Jeudi','Vendredi');
+    while ($ligne = $req->fetch())
+    {
+       $nom = $ligne['name'];
+        if($tmp != $nom) {
+            if($nbIndispo!=0) {
+
+                $xml.="    <Teacher>".$tmp."</Teacher>".PHP_EOL."    <Number_of_Not_Available_Times>".$nbIndispo."</Number_of_Not_Available_Times>".PHP_EOL;
+                for ($i=0;$i<count($tabDispo);$i++) {
+                    if(array_key_exists($tabJour[$i],$tabDispo)) {
+                        for($j=0;$j<count(array_keys($tabDispo[$tabJour[$i]]));$j++) {
+                            $xml.="    <Not_Available_Time>".PHP_EOL."        <Day>".$tabJour[$i]."</Day>".PHP_EOL."        <Hour>".$tabDispo[$tabJour[$i]][$j]."</Hour>".PHP_EOL."    </Not_Available_Time>".PHP_EOL;
+                        }
+                    }
+                }
+
+            } // if($nbIndispo!=0
+
+            $nbIndispo=0;
+            $tabDispo = array();
+            $tmp = $nom;
+            for ($i=8;$i<18;$i++) {
+                if($ligne['h'.$i]== -1 || $ligne['h'.$i]== 0) {
+                    $nbIndispo++;
+                    $tabDispo[$ligne['jour']][]= $i;
+                }
+            }
+            echo PHP_EOL." ".$tmp.PHP_EOL;
+        } // if($tmp != $nom)
+
+        else {
+            for ($i=8;$i<18;$i++) {
+                if($ligne['h'.$i]== -1 || $ligne['h'.$i]== 0) {
+                    $nbIndispo++;
+                    $tabDispo[$ligne['jour']][]= $i;
+                }
+            }
+        } // else
+
+    } //while
+    $xml.="    <Teacher>".$tmp."</Teacher>".PHP_EOL."    <Number_of_Not_Available_Times>".$nbIndispo."</Number_of_Not_Available_Times>".PHP_EOL;
+    for ($i=0;$i<count($tabDispo);$i++) {
+        if(array_key_exists($tabJour[$i],$tabDispo)) {
+            for($j=0;$j<count(array_keys($tabDispo[$tabJour[$i]]));$j++) {
+                $xml.="    <Not_Available_Time>".PHP_EOL."        <Day>".$tabJour[$i]."</Day>".PHP_EOL."        <Hour>".$tabDispo[$tabJour[$i]][$j]."</Hour>".PHP_EOL."    </Not_Available_Time>".PHP_EOL;
+            }
+        }
+    }
+
+    $xml.=" <Active>true</Active>".PHP_EOL."<Comments></Comments>".PHP_EOL."</ConstraintTeacherNotAvailableTimes>";
+     file_put_contents("dispo.xml",$xml);
+
+} // listeDispoProfs
 
 
 
+
+listeProfs();
+listeMatieres();
+listeDispoProfs();
+/*
+ $reponse = $bdd->query('SELECT * FROM subject LIMIT 0,5');
 while ($donnees = $reponse->fetch())
     {
     ?>
@@ -21,27 +141,9 @@ while ($donnees = $reponse->fetch())
         cnu : <?php echo $donnees['cnu']; ?>
     </p>
 <?php
-}
-//affichage des 5 premieres matieres ( pour tester l'acces a la BDD )
-
-$ress  = $bdd->query('SELECT * FROM subject LIMIT 0,10');
-
-$xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
-        while($ligne = $ress->fetch()){
-        $xml.="<item>".$ligne['name']."</item>";
-        }
-file_put_contents("matiere.xml",$xml);
-// generation du fichier xml
+}*/
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>index</title>
-</head>
-<body>
-<p>test</p>
 </body>
 </html>
 
